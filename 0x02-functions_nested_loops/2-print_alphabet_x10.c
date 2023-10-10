5579d37f80deb1b57e7d0e76317c03ef838c9736
Demonware023 <awamchimere@icloud.com>
2023-10-10 16:17:36 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 x alphabet
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char b;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+			_putchar(b);
+		-putchar('\n');
+	}
+}
