9585af3e90db4574db46b38366f08ee8dc6cab19
Demonware023 <awamchimere@icloud.com>
2023-10-10 17:13:36 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: function parameter
+ * Return: 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
