736127083895e48d67987a880bb294701a515e65
Demonware023 <awamchimere@icloud.com>
2023-10-12 13:31:37 -0700

    isupper

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase
+ * _isdigit - checks for a digit
  * @c: parameter to be checked
  * Return: 1 for success 0 otherwise
  */
 
-int _isupper(int c)
-
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	(if c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
