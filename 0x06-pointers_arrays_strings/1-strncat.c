3562b4f669b47416e916beb762eddfecb0c7e954
Demonware023 <awamchimere@icloud.com>
2023-10-19 09:03:17 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 	int srclen = 0;
 	int destlen = 0;
 
-	for (i = 0 ; src[i] != '\0' ; i++)
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		srclen++;
-	for (i = 0 ; dest[i] != '\0' ; i++)
+	for (i = 0 ; i < n && dest[i] != '\0' ; i++)
 		destlen++;
 
 	for (i = 0 ; srclen <= n ; i++)
