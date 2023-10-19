23e6e2a15ec9b91396e3b9ad998acbfa07c30f63
Demonware023 <awamchimere@icloud.com>
2023-10-19 13:28:34 -0700

    Strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,21 +5,20 @@
  * @dest: destination to be copied to
  * @src: source to be copied from
  * @n: bytes to be copied
- * Return: 0
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int srclen;
 
-	for (i = 0 ; src[i] != '\0' ; i++)
-		srclen++;
-	for (i = 0 ; i <= n && src[i] != '\0' ; i++)
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[i] = src[i];
-
-	for ( ; i < n; i++)
+	while (i < n)
+	{
 		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
