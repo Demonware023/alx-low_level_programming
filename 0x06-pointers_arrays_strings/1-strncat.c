ac89f206b9f355ff048cb046abeddca1db5f9a56
Demonware023 <awamchimere@icloud.com>
2023-10-19 08:23:23 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings with n bytes
+ * @src: length of source string
+ * @dest: length of destination string
+ * @n: most bytes to be used
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int srclen = 0;
+	int destlen = 0;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+
+	for (i = 0 ; srclen <= n ; i++)
+		dest[destlen + i] = src[i];
+
+	return (dest);
+}
