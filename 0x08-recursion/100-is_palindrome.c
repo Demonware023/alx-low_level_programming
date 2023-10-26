5d91ef1bca25d45585a2e4c4f20c7d4f473b78dd
Demonware023 <Awamchimere@icloud.com>
2023-10-26 11:11:39 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * is_palindrome - checks is the string is an empty string
+  *
+  * @s - string
+  *
+  * Return: 1 or 0
+  */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	if (*s != *(s + strlen(s) - 1))
+	{
+		return (0);
+	}
+
+	return (is_palindrome(s + 1));
+}
