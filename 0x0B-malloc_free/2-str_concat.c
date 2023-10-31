617c3c60c8ac4f2e5b15f6de418621b99455632e
Demonware023 <Awamchimere@icloud.com>
2023-10-31 09:28:11 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,9 +4,9 @@
 /**
   * *str_concat - a function that concatenates two strings using malloc
   *
-  * @s1 - string 1
+  * @s1: string 1
   *
-  * @s2 - string 2
+  * @s2: string 2
   *
   * Return: NULL or Success
   */
@@ -17,9 +17,15 @@ char *str_concat(char *s1, char *s2)
 	size_t len1;
 	size_t len2;
 
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
