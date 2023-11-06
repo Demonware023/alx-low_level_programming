78ef20034bcc80ba0a0e0ff818feb40579633fea
Demonware023 <Awamchimere@icloud.com>
2023-11-06 12:14:42 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG
+#define DOG
+
+/**
+  * struct dog - Define a new type with some elements
+  *
+  * @name: dog name
+  *
+  * @age: dog age
+  *
+  * @owner: dog owner
+  *
+  * Return: 0
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
