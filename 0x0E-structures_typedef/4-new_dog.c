c54c9d1c1a8251012e07d05fe5f5092c3def3ee4
Demonware023 <Awamchimere@icloud.com>
2023-11-06 15:27:09 -0800

    How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,6 +27,13 @@ char *strdup(const char *s)
 	return (d);
 }
 
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	   dog_t *newDog;
