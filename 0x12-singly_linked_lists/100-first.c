0bbb7882f405319a413f54f7c06f878dcbf5dc90
Demonware023 <awamchimere@icloud.com>
2023-11-16 15:11:56 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
-  * __attribute__((constructor)) - pre main funtion.
+  * pre_main - Pre main fucntion.
+  *
+  * __attribute__((constructor)): Prints before main is executed.
   *
   * Return: Always 0 Success.
   */
 
 void __attribute__((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
